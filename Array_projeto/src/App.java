b84b79572c5b5c80c232e5316c08f75ed36eb42e
ivsonferreira <ivsonferreirajr@gmail.com>
2022-04-17 12:38:41 -0300

    #

--- Array_projeto/src/App.java
+++ Array_projeto/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    Array array = new Array(10);
+    
+    for(int i = 0; i < 9; i++){
+        array.set(i, i);
     }
+
+    System.out.println(array.contains(0));
+    System.out.println(array.get(6));
+    System.out.println(array.indexOf(8));
+    System.out.println(array.size());
+}
 }
